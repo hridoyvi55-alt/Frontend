20b3ec0fe75df8179ca271f361f908e7accdb7d7
hridoyvi55-alt <hridoyvi55@gmail.com>
2026-05-10 03:33:58 +0400

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// তোমার Firebase config
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCvhGuyx6ZSrimCjk2G2Q9pdoihvjb3Hms",
+  authDomain: "realearn-app.firebaseapp.com",
+  databaseURL: "https://realearn-app-default-rtdb.firebaseio.com",
+  projectId: "realearn-app",
+  storageBucket: "realearn-app.firebasestorage.app",
+  messagingSenderId: "241034846565",
+  appId: "1:241034846565:web:26f6da801689f429f9018c",
+  measurementId: "G-WPH8TW221C"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+
+export default app;
